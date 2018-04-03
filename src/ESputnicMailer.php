388afe62d4eca07a0a91e57c74c20fe8eb1f8976
egretos <egretos@outlook.com>
2018-04-03 14:22:15 +0300

    ESputnicMailer in progress

--- src/ESputnicMailer.php
+++ src/ESputnicMailer.php
@@ -23,7 +23,7 @@ class ESputnicMailer extends BaseMailer
             \Yii::$app->params['esputnic.userpass']
         );
         $this->api = $api;
-        parent::__construct($config);
+        //parent::__construct($config);
     }
 
 
@@ -34,7 +34,54 @@ class ESputnicMailer extends BaseMailer
      */
     public function compose($view = null, array $params = [])
     {
-        return new ESputnicMessage();
+        $message = new ESputnicMessage();
+        if ($view === null) {
+            return $message;
+        }
+
+        if (!array_key_exists('message', $params)) {
+            $params['message'] = $message;
+        }
+
+        $this->_message = $message;
+
+        if (is_array($view)) {
+            if (isset($view['html'])) {
+                $html = $this->render($view['html'], $params, $this->htmlLayout);
+            }
+            if (isset($view['text'])) {
+                $text = $this->render($view['text'], $params, $this->textLayout);
+            }
+        } else {
+            $html = $this->render($view, $params, $this->htmlLayout);
+        }
+
+        $this->_message = null;
+
+        if (isset($html)) {
+            $message->setBody($html);
+        }
+        if (isset($text)) {
+            $message->setTextBody($text);
+        } elseif (isset($html)) {
+            if (preg_match('~<body[^>]*>(.*?)</body>~is', $html, $match)) {
+                $html = $match[1];
+            }
+            // remove style and script
+            $html = preg_replace('~<((style|script))[^>]*>(.*?)</\1>~is', '', $html);
+            // strip all HTML tags and decoded HTML entities
+            $text = html_entity_decode(
+                strip_tags($html),
+                ENT_QUOTES | ENT_HTML5,
+                Yii::$app ? Yii::$app->charset : 'UTF-8'
+            );
+            // improve whitespace
+            $text = preg_replace("~^[ \t]+~m", '', trim($text));
+            $text = preg_replace('~\R\R+~mu', "\n\n", $text);
+            $message->setTextBody($text);
+        }
+
+        return $message;
     }
 
     /**
